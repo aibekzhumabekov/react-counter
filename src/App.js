4452b362196fed2836f9c73387bb51f998dec4dd
Aybek <aybek.jumabek@gmail.com>
2020-01-21 02:24:16 -0600

    adding a decrement functionality and styling with grid, lastly making the button disabled

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   state = {
     counters: [
       { id: 1, value: 0 },
-      { id: 2, value: 2 },
+      { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 }
     ]
@@ -29,6 +29,15 @@ class App extends Component {
     counters[index].value++;
     this.setState({ counters });
   };
+
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
   handleReset = () => {
     const counters = this.state.counters.map(c => {
       c.value = 0;
@@ -54,6 +63,7 @@ class App extends Component {
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
           />
         </main>
