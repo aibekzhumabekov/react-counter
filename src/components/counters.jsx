dda9baf276558efe95b28d793aac100949b0eb24
Aybek <aybek.jumabek@gmail.com>
2020-01-21 00:08:05 -0600

    Raising and handling events aster updating the State

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,14 +10,20 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
           ></Counter>
         ))}
       </div>
